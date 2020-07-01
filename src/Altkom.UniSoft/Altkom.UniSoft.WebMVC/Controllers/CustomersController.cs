4705a738111a5844b1c4542100332b7c64290ba2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-01 10:50:16 +0200

    MVC

---
+++ src/Altkom.UniSoft/Altkom.UniSoft.WebMVC/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using Alktom.UniSoft.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altkom.UniSoft.WebMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ICustomerService customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = customerService.Get();
+
+            return View(customers);
+        }
+    }
+}
