4d45c7fcceb43fc4a687d8f91fc8421c6c5660cd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-01 14:40:36 +0200

    ViewComponent

---
+++ src/Altkom.UniSoft/Altkom.UniSoft.RazorPages/ViewComponents/CustomerViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Altkom.UniSoft.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altkom.UniSoft.RazorPages.ViewComponents
+{
+    public class CustomerViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke(Customer customer)
+        {
+            return View(customer);
+        }
+    }
+}
