8cf44fa7f44307a11e252448437b8810cc034456
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-02 11:44:21 +0200

    Order Model

---
+++ src/Altkom.UniSoft/Altkom.UniSoft.Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altkom.UniSoft.Models
+{
+    public class Order : Base
+    {
+        public string Number { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public Customer Customer { get; set; }
+        public OrderStatus Status { get; set; }
+        public ICollection<OrderDetail> Details { get; set; }
+
+        public Order()
+        {
+            Details = new List<OrderDetail>();
+        }
+    }
+
+    public class OrderDetail : Base
+    {
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Draft,
+        Ordered,
+        Canceled,
+        Done
+    }
+}
--- src/Altkom.UniSoft/Altkom.UniSoft.Models/Product.cs
+++ src/Altkom.UniSoft/Altkom.UniSoft.Models/Product.cs
@@ -1,10 +1,21 @@
-﻿namespace Altkom.UniSoft.Models
+﻿using System;
+
+namespace Altkom.UniSoft.Models
 {
-    public class Product : Base
+    public abstract class Item : Base
     {
-       
         public string Name { get; set; }
-        public string Color { get; set; }
         public decimal UnitPrice { get; set; }
+    }
+
+    public class Product : Item
+    {
+        public string Color { get; set; }
+    }
+
+    public class Service : Item
+    {        
+        public TimeSpan Period { get; set; }
+
     }
 }
