38bcf835d232b350a5b163cb10a9f22abd7a9aa9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-07-03 14:02:18 +0200

    HostFactory

---
+++ src/Altkom.UniSoft/Altkom.UniSoft.WindowsService/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Topshelf;
+
+namespace Altkom.UniSoft.WindowsService
+{
+
+    // dotnet add package Topshelf
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            .Run(s =>
+            {
+                s.Service<LoggingService>();
+                s.SetServiceName("Altkom Service");
+                s.StartAutomatically();                
+            });
+        }
+    }
+
+    public class LoggingService : ServiceControl
+    {
+        private const string filename = @"c:\temp\log.txt";
+
+        private void Log(string message)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(filename));
+            File.AppendAllText(filename, $"{message} {DateTime.UtcNow} \n");
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Log("Started!");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Log("Stopped.");
+            return true;
+        }
+    }
+}
---
+++ src/Altkom.UniSoft/Altkom.UniSoft.WorkerService/Worker.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Altkom.UniSoft.WorkerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+        
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                File.AppendAllText("log.txt", $"Worker running at: {DateTimeOffset.Now} \n");
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
